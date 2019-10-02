abeb37c5400eaad03c90c5414ecd77135965e703
shangsuru <helm.henry99@gmail.com>
2019-10-02 07:53:13 +0200

    scraping data from all car listings on ebay

---
+++ models/car.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const CarSchema = new Schema({
+  brand: String,
+  carType: String,
+  color: String,
+  fuelType: String,
+  mileage: Number,
+  model: String,
+  price: String,
+  ps: Number,
+  yearOfRegistration: Number
+})
+
+const Car = mongoose.model("car", CarSchema);
+
+module.exports = Car;
