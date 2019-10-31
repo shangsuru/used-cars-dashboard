39459564b55cc33d8bf02de284401c1089929148
shangsuru <helm.henry99@gmail.com>
2019-10-31 13:47:54 +0100

    deploy

--- app.js
+++ app.js
@@ -1,36 +1,39 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const app = express();
-const cors = require("cors");
-const path = require("path");
-const routes = require("./routes/routes");
-const scrape = require("./public/scrape.js");
-const Car = require("./models/car");
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express()
+const cors = require('cors')
+const path = require('path')
+const routes = require('./routes/routes')
+const scrape = require('./public/scrape.js')
+const Car = require('./models/car')
 
-require("dotenv").config()
+require('dotenv').config()
 
-mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || process.env.MONGOHQ_URL || process.env.MONGOLAB_URI|| "mongodb://localhost/ebay", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+mongoose.Promise = global.Promise
+mongoose.connect(
+  'mongodb://heroku_56pbblnw:54cerqlj1pikrrgt1s6eo0i690@ds241688.mlab.com:41688/heroku_56pbblnw',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+)
 
-app.use(express.static(path.join(__dirname, "public")));
-app.use(express.static(path.join(__dirname, "client", "build")))
-app.use(cors());
-routes(app);
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'client', 'build')))
+app.use(cors())
+routes(app)
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-});
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+})
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8000
 app.listen(PORT, () => {
-  console.log("Running on port " + PORT);
-});
+  console.log('Running on port ' + PORT)
+})
 
 setInterval(() => {
-  Car.remove({}).then(scrape());
-}, 86400000);
+  Car.remove({}).then(scrape())
+}, 86400000)
 
-module.exports = app;
+module.exports = app
